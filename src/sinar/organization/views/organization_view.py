8243313cc52a691b07b2edbbb9dcf0df2cfdc4f9
Khairil Yusof <khairil.yusof@gmail.com>
2022-10-11 10:58:39 +0800

    Add lookup for vocabulary title for organization type in template

--- src/sinar/organization/views/organization_view.py
+++ src/sinar/organization/views/organization_view.py
@@ -3,19 +3,30 @@
 # from sinar.organization import _
 from plone.dexterity.browser.view import DefaultView
 from zope.interface import Interface
-
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
 
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-
 class IOrganizationView(Interface):
     """ Marker Interface for IOrganizationView"""
-
 
 class OrganizationView(DefaultView):
     # If you want to define a template here, please remove the template from
     # the configure.zcml registration of this view.
     # template = ViewPageTemplateFile('organization_view.pt')
+
+    def organization_type_title(self):
+
+        factory = getUtility(IVocabularyFactory,
+                             'sinar.organization.OrganizationType')
+
+        vocabulary = factory(self)
+
+        term = vocabulary.getTerm(self.context.organization_type)
+
+        return term.title
+
 
     def __call__(self):
         # Implement your own actions:
