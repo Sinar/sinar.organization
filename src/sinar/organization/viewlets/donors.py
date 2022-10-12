7bfa659a3d5203ec5cd25e9b79434957ff7dc236
Khairil Yusof <khairil.yusof@gmail.com>
2022-10-12 16:20:47 +0800

    Add Donors viewlet showing logo/link of supporting/funding organizations

--- src/sinar/organization/viewlets/donors.py
+++ src/sinar/organization/viewlets/donors.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
+from plone import api
 from plone.app.layout.viewlets import ViewletBase
-
+from plone.app.uuid.utils import uuidToObject
 
 class Donors(ViewletBase):
 
-    def update(self):
-        self.message = self.get_message()
+    def donors(self):
 
-    def get_message(self):
-        return u'My message'
+        objects = []
+        partners = self.context.donors
+        
+        if partners:
+            for partner in partners:
+                obj = uuidToObject(partner)
+                objects.append(obj)
+
+        return objects
+
 
     def index(self):
         return super(Donors, self).render()
