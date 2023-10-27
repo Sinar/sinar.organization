37cc8b2f20f4de93b8546cf8667536c9d270b1fe
sitinurliza <siti.nurliza@sinarproject.org>
2023-10-27 13:26:23 +0800

    "Add behavior: ContactDetails"

--- src/sinar/organization/viewlets/donors.py
+++ src/sinar/organization/viewlets/donors.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from plone.app.layout.viewlets import ViewletBase
 from collective.relationhelpers import api
+from plone.app.layout.viewlets import ViewletBase
+
 
 class Donors(ViewletBase):
 
